1c818a20eab1b9f9d8ebb5f7d75f9b0ae78c189f
Júlia Viana <juliavianamendes05@gmail.com>
2022-11-18 08:10:12 -0300

    praticando exercicios

---
+++ trybe_exercises/Front-end/secao-2-introducao-ao-react/hello-world-react/src/App.js
@@ -0,0 +1,8 @@
+import './App.css';
+import  HelloWorld from './HelloWorld';
+
+function App() {
+  return ( <spa className ="HelloWorld">Hi there</spa>);
+}
+
+export default App;
---
+++ trybe_exercises/Front-end/secao-2-introducao-ao-react/hello-world-react/src/HelloWorld.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+class HelloWorld extends React.Component {
+    render(){
+return <span className="HelloWorld">Hello, world!</span>
+    }
+}
+export default HelloWorld;
