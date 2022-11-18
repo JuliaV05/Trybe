484c8ee95ee916a8df642529acf8ab0ac3596f51
Júlia Viana <juliavianamendes05@gmail.com>
2022-11-18 08:41:48 -0300

    desenvolvendo para fixar

--- trybe_exercises/Front-end/secao-6-testes-automatizados-react-testing-library/testes-react/src/App.test.js
+++ trybe_exercises/Front-end/secao-6-testes-automatizados-react-testing-library/testes-react/src/App.test.js
@@ -1,23 +1,23 @@
 // App.test.js
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import App from './App'; 
+import userEvent from'@testing-library/user-event';
 
-test('Verificando se existe o campo Email.', () => {
+test('Verificando se o botão e o campo email estão funcionando.', () => {
   render(<App />);
-  const inputEmail = screen.getByLabelText('Email');
-  expect(inputEmail).toBeInTheDocument();
-  expect(inputEmail).toHaveProperty('type', 'email');
-});
-test('Verificando se existe um botão', () => {
-  render(<App />);
-  const btns = screen.getAllByRole('button');
-  expect(btns).toHaveLength(2);
-});
-test('Verificando se existe um botão de enviar', () => {
-  render(<App />);
+
+  const EMAIL_USER = 'email@email.com';
+
+  const textEmail = screen.getByTestId('id-email-user');
+  expect(textEmail).toBeInTheDocument();
+  expect(textEmail).toHaveTextContent('Valor:');
+
   const btnSend = screen.getByTestId('id-send');
-  expect(btnSend).toBeInTheDocument();
-  expect(btnSend).toHaveProperty('type', 'button');
-  expect(btnSend).toHaveValue('Enviar');
+  const inputEmail = screen.getByLabelText('Email');
+  userEvent.type(inputEmail, EMAIL_USER);
+  userEvent.click(btnSend);
+
+  expect(inputEmail).toHaveValue('');
+  expect(textEmail).toHaveTextContent(`Valor: ${ EMAIL_USER }`);
 });
